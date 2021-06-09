7bb02701222c453ddcbc57964cbb37938f97dc4a
Victor Miti <victormiti@umusebo.com>
2021-06-09 10:58:39 +0200

    feat: use latest Node.js LTS version instead of 12.x

--- setup.sh
+++ setup.sh
@@ -177,7 +177,7 @@ function setupHostname() {
 
 function setupNodeYarn() {
   # nodeJS
-  curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+  curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
   sudo apt-get install -y nodejs
 
   # yarn

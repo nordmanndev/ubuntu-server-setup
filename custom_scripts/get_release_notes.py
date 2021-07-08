80a5d76847c56b5c8df7d64a3f85040f6c21bd0e
Victor Miti <victormiti@umusebo.com>
2021-07-08 21:59:52 +0200

    ci: automation of release process

---
+++ custom_scripts/get_release_notes.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+"""get_release_notes.py
+
+a small script that reads the CHANGELOG.md and extracts
+the content for the latest release.
+
+this is meant to be used in a CI/CD setup for automated
+GitHub releases.
+"""
+
+import os
+from pathlib import Path
+
+__author__ = "Victor Miti"
+__copyright__ = "Copyright 2021, Victor Miti"
+__license__ = "MIT"
+
+
+def get_release_notes():
+    """extract content from CHANGELOG.md for use in Github Releases
+
+    we read the file and loop through line by line
+    we wanna extract content beginning from the first Heading 2 text
+    to the last line before the next Heading 2 text
+    """
+    pattern_to_match = "## [v"
+
+    count = 0
+    lines = []
+    heading_text = "## What's changed in this release\n"
+    lines.append(heading_text)
+
+    with open("CHANGELOG.md", "r") as c:
+        for line in c:
+            if pattern_to_match in line and count == 0:
+                count += 1
+            elif pattern_to_match not in line and count == 1:
+                lines.append(line)
+            elif pattern_to_match in line and count == 1:
+                break
+
+    home = str(Path.home())
+    release_notes = os.path.join(home, "LATEST_RELEASE_NOTES.md")
+    with open(release_notes, "w") as f:
+        print("".join(lines), file=f, end="")
+
+
+if __name__ == "__main__":
+    get_release_notes()
---
+++ custom_scripts/release.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+
+"""release.py
+
+to help in managing releases using `standard-version`_.
+
+.. _standard-version: https://github.com/conventional-changelog/standard-version
+"""
+
+import argparse
+import os
+import subprocess
+import sys
+
+__author__ = "Victor Miti"
+__copyright__ = "Copyright 2021, Victor Miti"
+__license__ = "MIT"
+
+
+def release(args=None):
+    """Console script entry point"""
+
+    if not args:
+        args = sys.argv[1:]
+
+    parser = argparse.ArgumentParser(
+        prog="release",
+        description="to help in managing releases using standard-version",
+    )
+
+    parser.add_argument(
+        "release_type", help="The type of release [ minor | major | patch ].", type=str
+    )
+
+    args = parser.parse_args(args)
+
+    if args.release_type in ["major", "minor", "patch"]:
+        print("let me retrieve the tag we're bumping from ...")
+        get_current_tag = subprocess.getoutput(
+            "git describe --abbrev=0 --tags `git rev-list --tags --skip=0  --max-count=1`"
+        )
+        previous_tag = get_current_tag.strip("\n")
+        # now we can pass result to standard-release
+        os.system(
+            f'npm run release -- --commit-all --release-as {args.release_type} --releaseCommitMessageFormat "bump: ✈️ {previous_tag} → v{{{{currentTag}}}}"'
+        )
+        # push to origin
+        os.system("git push --follow-tags origin master")
+    else:
+        print("accepted release types: minor | major | patch")
+        print("please try again")
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    unstaged_str = "not staged for commit"
+    uncommitted_str = "to be committed"
+    check = subprocess.getoutput("git status")
+    if unstaged_str not in check or uncommitted_str not in check:
+        release()
+    else:
+        print("Sorry mate, please ensure there are no pending git operations")

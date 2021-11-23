efa6043e7137c938b9441d120172fc37544c8c3e
Victor Miti <victormiti@umusebo.com>
2021-11-23 12:22:37 +0200

    refactor: explicitly specify node 14.x instead of latest lts
    not ready for 16.x -- some apps break as a result

--- setup.sh
+++ setup.sh
@@ -178,7 +178,7 @@ function setupHostname() {
 
 function setupNodeYarn() {
   # nodeJS
-  curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
+  curl -fsSL https://deb.nodesource.com/setup_14.x | sudo -E bash -
   sudo apt-get install -y nodejs
 
   # yarn
